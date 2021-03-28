4fd3ab906816fbf2eae6026aab2c41fa5159a994
UnDuhDuhSea <tafimba@gmail.com>
2021-03-27 20:11:31 -0700

    changed function for pulling from API

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var weatherBtn = $('#button-addon2')
+var userForm = $('#user-form')
+var cityName = $("#city-name");
+var weatherBtn = $('#button-addon2');
 var inputValue = $(".searchInput");
 var temp = $("#temperature");
 var humidity = $("#humidity");
@@ -21,20 +23,87 @@ var day1Temp = $("#day5-temp")
 var day1Humid = $("#day5-humid")
 
 
-weatherBtn.on("click", function () {
-    fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + inputValue.val() + '&appid=eb5223f28d4380631ba895c1f6de4c48')
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .then(data => populateWeather)
-        .catch(err => alert("Wrong city name!"))
-})
+// get search string
+var querySubmit = function (event) {
+    event.preventDefault();
+
+    var searchCity = inputValue.val().trim();
+
+    if (searchCity) {
+        getWeather(searchCity);
+
+        inputValue.value = '';
+    } else {
+        alert('Please enter a valid city.')
+    }
+}
+
+var getWeather = function (query) {
+    var apiUrl = "https:api.openweathermap.org/data/2.5/forecast?q=" + query + "&units=imperial&appid=eb5223f28d4380631ba895c1f6de4c48";
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function (data) {
+                console.log(data);
+            });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
+    })
+        .catch(function (error) {
+            alert('Unable to connect to OpenweatherAPI')
+        })
+};
+userForm.on('submit', querySubmit);
+
+
+
+
+
+
+
+
+
+// ########################### SECOND ATTEMPT WITH CODE FROM VIDEO ########################################
+// function renderWeather(weather) {
+//     var nameVal = weather.name.val()
+//     console.log(nameVal)
+// }
+
+// // Fetch weather data for city
+// function fetchWeather(query) {
+//     var url = "https:api.openweathermap.org/data/2.5/forecast?q=" + query + "&units=imperial&appid=eb5223f28d4380631ba895c1f6de4c48";
+
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => console.log(data));
+// }
+
+// fetchWeather();
+
+
+
+// // ###################### FIRST ATTEMPT ##############################################
+// weatherBtn.on("click", function () {
+//     fetch('https:api.openweathermap.org/data/2.5/forecast?q=' + inputValue.val() + '&units=imperial&appid=eb5223f28d4380631ba895c1f6de4c48')
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+//         .then(data => {
+//             var city = document.createElement("h2");
+//             city.textContent = data.city.name.val();
+//             cityName.append(city)
+//         })
+//         .catch(alert('this is wrong'))
+// })
 
 // function to dispurse weather information
 
-function populateWeather() {
-    var tempValue = 
-}
-
+// function populateWeather() {
+//     var tempVal = data.list.main.temp
+//     console.log(tempVal)
+// }
+// populateWeather()
 
 // fetch('api.openweathermap.org/data/2.5/forecast?q=' + inputValue.value + '&appid = eb5223f28d4380631ba895c1f6de4c48')
 //     .then(response => response.json())
