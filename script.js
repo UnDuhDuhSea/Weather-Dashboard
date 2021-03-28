3884fe2a0d592fb34cdd0eb67ed2794c1a8969c6
UnDuhDuhSea <tafimba@gmail.com>
2021-03-28 16:00:04 -0700

    added 5 day forcast pull from API

--- script.js
+++ script.js
@@ -2,27 +2,33 @@ var userForm = $('#user-form')
 var cityName = $("#city-name");
 var weatherBtn = $('#button-addon2');
 var inputValue = $(".searchInput");
-var temp = $("#temperature");
+var temp = $("#temp");
 var humidity = $("#humidity");
 var windSpeed = $("#wind-speed");
 var uvIndex = $("#uv-index");
+var day1Date = $("#day1-date")
 var day1IMG = $("#day1-img");
 var day1Temp = $("#day1-temp")
 var day1Humid = $("#day1-humid")
-var day1IMG = $("#day2-img");
-var day1Temp = $("#day2-temp")
-var day1Humid = $("#day2-humid")
-var day1IMG = $("#day3-img");
-var day1Temp = $("#day3-temp")
-var day1Humid = $("#day3-humid")
-var day1IMG = $("#day4-img");
-var day1Temp = $("#day4-temp")
-var day1Humid = $("#day4-humid")
-var day1IMG = $("#day5-img");
-var day1Temp = $("#day5-temp")
-var day1Humid = $("#day5-humid")
+var day2Date = $("#day2-date")
+var day2IMG = $("#day2-img");
+var day2Temp = $("#day2-temp")
+var day2Humid = $("#day2-humid")
+var day3Date = $("#day3-date")
+var day3IMG = $("#day3-img");
+var day3Temp = $("#day3-temp")
+var day3Humid = $("#day3-humid")
+var day4Date = $("#day4-date")
+var day4IMG = $("#day4-img");
+var day4Temp = $("#day4-temp")
+var day4Humid = $("#day4-humid")
+var day5Date = $("#day5-date")
+var day5IMG = $("#day5-img");
+var day5Temp = $("#day5-temp")
+var day5Humid = $("#day5-humid")
 
-
+// declare empty array for local storage
+var citiesSearched = [];
 // get search string
 var querySubmit = function (event) {
     event.preventDefault();
@@ -59,22 +65,46 @@ var getWeather = function (query) {
 
 function renderWeather(weather) {
     console.log(weather)
-    cityName.append(`<span>${weather.city.name}</span>`);
-    console.log(test)
-    // var city = cityName.createElement('span');
-    // console.log(city)
-    // city.textContent = weather.city.name;
-    // cityName.append(city);
+
+    // Day 1 Weather
+    day1Date.append(`<span>${weather.list[3].dt_txt.slice(0, 10)}</span>`);
+    day1IMG.append(`<span>${weather.list[3].weather[0].icon}</span>`);
+    day1Temp.append(`<span>${weather.list[3].main.temp}</span>`);
+    day1Humid.append(`<span>${weather.list[3].main.humidity}</span>`);
+    // Day 2 Weather
+    day2Date.append(`<span>${weather.list[11].dt_txt.slice(0, 10)}</span>`);
+    day2IMG.append(`<span>${weather.list[11].weather[0].icon}</span>`);
+    day2Temp.append(`<span>${weather.list[11].main.temp}</span>`);
+    day2Humid.append(`<span>${weather.list[11].main.humidity}</span>`);
+    // Day 3 Weather
+    day3Date.append(`<span>${weather.list[19].dt_txt.slice(0, 10)}</span>`);
+    day3IMG.append(`<span>${weather.list[19].weather[0].icon}</span>`);
+    day3Temp.append(`<span>${weather.list[19].main.temp}</span>`);
+    day3Humid.append(`<span>${weather.list[19].main.humidity}</span>`);
+    // Day 4 Weather
+    day4Date.append(`<span>${weather.list[27].dt_txt.slice(0, 10)}</span>`);
+    day4IMG.append(`<span>${weather.list[27].weather[0].icon}</span>`);
+    day4Temp.append(`<span>${weather.list[27].main.temp}</span>`);
+    day4Humid.append(`<span>${weather.list[27].main.humidity}</span>`);
+    // Day 5 Weather
+    day5Date.append(`<span>${weather.list[35].dt_txt.slice(0, 10)}</span>`);
+    day5IMG.append(`<span>${weather.list[35].weather[0].icon}</span>`);
+    day5Temp.append(`<span>${weather.list[35].main.temp}</span>`);
+    day5Humid.append(`<span>${weather.list[35].main.humidity}</span>`);
 }
-
-
-
-
 userForm.on('submit', querySubmit);
 
 
 
 
+
+
+// information for next call from API
+// cityName.append(`<span>${weather.city.name}</span>`);
+// temp.append(`<span>${weather.list[0].main.temp}</span>`);
+// humidity.append(`<span>${weather.list[0].main.humidity}</span>`);
+// windSpeed.append(`<span>${weather.list[0].wind.speed}</span>`);
+// uvIndex.append(`<span>${weather.list[0].wind.speed}</span>`);
 
 
 
